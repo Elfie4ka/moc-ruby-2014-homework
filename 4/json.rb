f9edf350bf7198e1545d39926a53d4883c486223
nataliia <nataliia.chausova@gmail.com>
2014-12-20 20:52:07 +0200

    new json

--- 4/json.rb
+++ 4/json.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'json'
 
-#RESPONSE='{"person":{"personal_data":{"name": 'Nataly', "gender":'female', "age":18},"social_profiles":['https://www.facebook.com/janataliia'],'additional_info':{'hobby':['skiing', 'travelling']}}'
-
+RESPONSE='{"person":{"personal_data":{"name": "Nataly", "gender":"female", "age":21},
+"social_profiles":["http://www.facebook.com/janataliia"],
+"additional_info":{"hobby":[], "pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}}}'
 
 response = JSON.parse(RESPONSE, :quirks_mode => true)
 
 Person = Struct.new(*response["person"].keys.collect(&:to_sym)) do
+
   def greeting
     if (defined? personal_data)&&(personal_data['name'])
       name=personal_data['name']
@@ -25,27 +27,15 @@ Person = Struct.new(*response["person"].keys.collect(&:to_sym)) do
     end
   end
 
-  def facebook_addr
-    if defined? social_profiles
-      social_profiles.each do |social_profile|
-        puts social_profile.scan(/(?:[-a-z_\d])+'facebook'(?:[-a-z])*(?:\.[a-z]{2,4})+/)
-      end
-  end
-
   def hobbies?
-    has_hobby = false
-    if defined?(additional_info) && additional_info['hobby']
-      has_hobby = true
-    end
-    has_hobby
+    puts additional_info['hobby']
+    additional_info['hobby'].to_s.empty?
   end
+end
 
-person = Person.new(*response["person"].values)
+person = Person.new(* response["person"].values)
 person.greeting
 p person.adult?
 person.gender
-person.facebook_addr
+#p person.facebook_addr
 p person.hobbies?
-
-end
-end
---
+++ 4/json2.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'json'
+
+RESPONSE='{"person":{"personal_data":{"name": "Nataly", "gender":"female", "age":18},
+"social_profiles":["https://www.facebook.com/janataliia"],
+"additional_info":{"hobby":["skiing", "travelling"]}}}'
+
+response = JSON.parse(RESPONSE, :quirks_mode => true)
+
+Person = Struct.new(*response["person"].keys.collect(&:to_sym)) do
+    def greeting
+        if (defined? personal_data)&&(personal_data['name'])
+            name=personal_data['name']
+            puts "Hello #{name}!"
+        end
+    end
+
+    def adult?
+        personal_data["age"] >= 21
+    end
+
+    def gender
+        if (defined? personal_data)&&(personal_data['gender'])
+            puts (personal_data["gender"])
+        else puts "The user didn't say"
+        end
+    end
+
+    def facebook_addr
+        if defined? social_profiles
+            social_profiles.each do |social_profile|
+            end
+        end
+    end
+
+    def hobbies?
+        has_hobby = false
+        if defined?(additional_info) && additional_info['hobby']
+            has_hobby = true
+        end
+        has_hobby
+    end
+end
+
+person = Person.new(*response["person"].values)
+person.greeting
+p person.adult?
+person.gender
+person.facebook_addr
+p person.hobbies?
